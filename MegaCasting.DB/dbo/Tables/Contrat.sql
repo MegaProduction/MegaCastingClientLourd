1ce9a007c28d941c9229f34f56748f2b347c8f29
Damien Buchet <damienbuchetdb@gmail.com>
2020-10-16 10:13:29 +0200

    Edit Background

---
+++ MegaCasting.DB/dbo/Tables/Contrat.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Contrat] (
+    [Identifiant] INT           IDENTITY (1, 1) NOT NULL,
+    [Libelle]     NVARCHAR (50) NOT NULL,
+    CONSTRAINT [PK_Contrat] PRIMARY KEY CLUSTERED ([Identifiant] ASC)
+);
+
---
+++ MegaCasting.DB/dbo/Tables/Ville.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Ville] (
+    [Identifiant]     INT           IDENTITY (1, 1) NOT NULL,
+    [Libelle]         NVARCHAR (50) NOT NULL,
+    [CodePostal]      NVARCHAR (10) NOT NULL,
+    [IdentifiantPays] INT           NULL,
+    CONSTRAINT [PK_Ville] PRIMARY KEY CLUSTERED ([Identifiant] ASC),
+    CONSTRAINT [FK_Pays_Pays] FOREIGN KEY ([IdentifiantPays]) REFERENCES [dbo].[Pays] ([Identifiant])
+);
+
