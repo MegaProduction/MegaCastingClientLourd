3c70e29a7fc711aca74f432927e715a00d049731
Damien Buchet <damienbuchetdb@gmail.com>
2020-10-16 14:33:10 +0200

    Connection String

---
+++ MegaCasting.WPF/ViewModel/ViewModelAddOffer.cs
@@ -0,0 +1,80 @@
+﻿using MegaCasting.DBlib;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MegaCasting.WPF.ViewModel
+{
+    class ViewModelAddOffer : ViewModelBase
+    {
+		#region Attrributes
+		/// <summary>
+		/// Collection d'offre
+		/// </summary>
+		private ObservableCollection<Offre> _Offres;
+		/// <summary>
+		/// 
+		/// </summary>
+		private Offre _SelectedOffre;
+		#endregion
+
+		#region Properties
+		/// <summary>
+		/// Obtient ou défini la collection d'offre
+		/// </summary>
+		public ObservableCollection<Offre> Offres
+		{
+			get { return _Offres; }
+			set { _Offres = value; }
+		}
+
+		/// <summary>
+		/// Obtient ou défini l'offre sélectionné
+		/// </summary>
+		public Offre SelectedOffre
+		{
+			get { return _SelectedOffre; }
+			set { _SelectedOffre = value; }
+		}
+        #endregion
+
+        #region Constructors
+        public ViewModelAddOffer(MegaCastingEntities entities)
+			: base(entities)
+		{
+			this.Offres = new ObservableCollection<Offre>();
+			foreach (Offre offre in this.Entities.Offres)
+			{
+				this.Offres.Add(offre);
+			}
+		}
+		#endregion
+
+		#region Methods
+		public void SaveChanges()
+		{
+			this.Entities.SaveChanges();
+		}
+
+		/// <summary>
+		/// Ajoute une nouvelle offre
+		/// </summary>
+		public void AddOffer()
+		{
+			if (this.Entities.Offres.Any(type => type.Intitule == "Nouvelle offre"))
+			{
+				Offre offre = new Offre();
+				offre.Intitule = "Nouvelle offre";
+
+				this.Offres.Add(offre);
+				this.SaveChanges();
+				this.SelectedOffre = offre;
+			}
+		}
+		#endregion
+
+	}
+}
