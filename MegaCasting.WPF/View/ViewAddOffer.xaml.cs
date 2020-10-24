99a3d3d825636c1bd0c29249a8438da58e93c462
Alexandre BREAULT <a.breault@iia-laval.fr>
2020-10-24 19:10:40 +0200

    edit view

--- MegaCasting.WPF/View/ViewAddOffer.xaml.cs
+++ MegaCasting.WPF/View/ViewAddOffer.xaml.cs
@@ -25,6 +25,94 @@ namespace MegaCasting.WPF.View
         {
             InitializeComponent();
         }
+        private void TextBoxIntitule_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBoxIntitule.Text = string.Empty;
+            TextBoxIntitule.GotFocus -= TextBoxIntitule_GotFocus;
+        }
+
+        private void TextBoxIntitule_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (TextBoxIntitule.Text.Trim().Equals(string.Empty))
+            {
+                TextBoxIntitule.Text = "Intitulé";
+                TextBoxIntitule.GotFocus += TextBoxIntitule_GotFocus;
+            }
+        }
+
+        private void TextBoxNbPostes_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBoxNbPostes.Text = string.Empty;
+            TextBoxNbPostes.GotFocus -= TextBoxNbPostes_GotFocus;
+        }
+
+        private void TextBoxNbPostes_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (TextBoxNbPostes.Text.Trim().Equals(string.Empty))
+            {
+                TextBoxNbPostes.Text = "Nombre de postes";
+                TextBoxNbPostes.GotFocus += TextBoxNbPostes_GotFocus;
+            }
+        }
+
+        private void TextBoxDescripPoste_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBoxDescripPoste.Text = string.Empty;
+            TextBoxDescripPoste.GotFocus -= TextBoxDescripPoste_GotFocus;
+        }
+
+        private void TextBoxDescripPoste_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (TextBoxDescripPoste.Text.Trim().Equals(string.Empty))
+            {
+                TextBoxDescripPoste.Text = "Description du poste";
+                TextBoxDescripPoste.GotFocus += TextBoxDescripPoste_GotFocus;
+            }
+        }
+
+        private void TextBoxDescripProfil_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBoxDescripProfil.Text = string.Empty;
+            TextBoxDescripProfil.GotFocus -= TextBoxDescripProfil_GotFocus;
+        }
+
+        private void TextBoxDescripProfil_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (TextBoxDescripProfil.Text.Trim().Equals(string.Empty))
+            {
+                TextBoxDescripProfil.Text = "Description du profil";
+                TextBoxDescripProfil.GotFocus += TextBoxDescripProfil_GotFocus;
+            }
+        }
+
+        private void TextBoxDureeDiff_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBoxDureeDiff.Text = string.Empty;
+            TextBoxDureeDiff.GotFocus -= TextBoxDureeDiff_GotFocus;
+        }
+
+        private void TextBoxDureeDiff_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (TextBoxDureeDiff.Text.Trim().Equals(string.Empty))
+            {
+                TextBoxDureeDiff.Text = "Durée de diffusion";
+                TextBoxDureeDiff.GotFocus += TextBoxDureeDiff_GotFocus;
+            }
+        }
+        private void TextBoxCoord_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBoxCoord.Text = string.Empty;
+            TextBoxCoord.GotFocus -= TextBoxCoord_GotFocus;
+        }
+
+        private void TextBoxCoord_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (TextBoxCoord.Text.Trim().Equals(string.Empty))
+            {
+                TextBoxCoord.Text = "Coordonnées";
+                TextBoxCoord.GotFocus += TextBoxCoord_GotFocus;
+            }
+        }
         /// <summary>
         /// Défini le comportement lors du clique sur le bouton d'ajout
         /// </summary>
@@ -33,7 +121,7 @@ namespace MegaCasting.WPF.View
         private void AddOffre_Click(object sender, RoutedEventArgs e)
         {
             //intitule de l'offre
-            string intitule = textBoxIntitule.Text.ToString();
+            string intitule = TextBoxIntitule.Text.ToString();
             bool ville = false;
             bool contrat = false;
             bool client = false;
