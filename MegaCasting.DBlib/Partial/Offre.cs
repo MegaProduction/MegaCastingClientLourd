35eb2231dc8c8a63b0d8925be15f84e837a81870
Alexandre BREAULT <a.breault@iia-laval.fr>
2020-11-14 01:44:51 +0100

    start edit views

---
+++ MegaCasting.DBlib/Partial/Offre.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MegaCasting.DBlib
+{
+    public partial class Offre
+    {
+        public string NameClient => OffreClients.Where(offre => offre.IdentifiantOffre == Identifiant).Select(offre => offre.Client.Libelle).FirstOrDefault();
+        public int NumberCandidat => Postules.Where(postule => postule.IdentifiantOffre == Identifiant).Count();
+    }
+}
--- MegaCasting.WPF/ViewModel/ViewModelHistoriqueOffer.cs
+++ MegaCasting.WPF/ViewModel/ViewModelHistoriqueOffer.cs
@@ -10,15 +10,19 @@ namespace MegaCasting.WPF.ViewModel
 {
     class ViewModelHistoriqueOffer : ViewModelBase
     {
+		private OffreClient _SelectedOffreClient;
+
+		public OffreClient SelectedOffreClient
+		{
+			get { return _SelectedOffreClient; }
+			set { _SelectedOffreClient = value; }
+		}
+
 		#region Attributes
 		/// <summary>
 		/// Collection d'offre
 		/// </summary>
 		private ObservableCollection<Offre> _Offres;
-		/// <summary>
-		/// 
-		/// </summary>
-		private Offre _SelectedOffre;
 		#endregion
 		#region Properties
 		/// <summary>
@@ -29,31 +33,13 @@ namespace MegaCasting.WPF.ViewModel
 			get { return _Offres; }
 			set { _Offres = value; }
 		}
-		/// <summary>
-		/// Obtient ou défini l'offre sélectionné
-		/// </summary>
-		public Offre SelectedOffre
-		{
-			get { return _SelectedOffre; }
-			set { _SelectedOffre = value; }
-		}
-		#endregion
-		private ObservableCollection<OffreClient> _OffresClients;
-
-		public ObservableCollection<OffreClient> OffreClients
-		{
-			get { return _OffresClients; }
-			set { _OffresClients = value; }
-		}
-
-		#region Construteur
-		public ViewModelHistoriqueOffer(MegaCastingEntities entities)
+        #endregion
+        #region Construteur
+        public ViewModelHistoriqueOffer(MegaCastingEntities entities)
 			: base(entities)
 		{
 			this.Entities.Offres.ToList();
 			this.Offres = this.Entities.Offres.Local;
-			this.Entities.OffreClients.ToList();
-			this.OffreClients = this.Entities.OffreClients.Local;
 		}
 		#endregion
 	}
